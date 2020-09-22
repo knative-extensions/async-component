7621e295d44d53222262ffb70b39ea90a8059b09
Belinda Vennam <beemarie@us.ibm.com>
2020-09-21 20:19:50 -0400

    adding test coverage

--- cmd/producer/main_test.go
+++ cmd/producer/main_test.go
@@ -14,6 +14,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -37,39 +38,45 @@ func TestAsyncRequestHeader(t *testing.T) {
 		name             string
 		async            bool
 		method           string
-		largeBody        bool
+		bodyType         string
 		contentLengthSet bool
 		returncode       int
 	}{{
 		name:       "async get request",
 		async:      true,
 		method:     "GET",
-		largeBody:  false,
+		bodyType:   "none",
 		returncode: 202,
 	}, {
 		name:       "non async get request",
 		async:      false,
 		method:     "GET",
-		largeBody:  false,
+		bodyType:   "none",
 		returncode: 200,
 	}, {
 		name:       "non async post request",
 		async:      false,
 		method:     "POST",
-		largeBody:  false,
+		bodyType:   "normal",
 		returncode: 200,
 	}, {
 		name:       "async post request with too large payload",
 		async:      true,
 		method:     "POST",
-		largeBody:  true,
+		bodyType:   "large",
 		returncode: 500,
 	}, {
 		name:       "async post request with smaller than limit payload",
 		async:      true,
 		method:     "POST",
-		largeBody:  false,
+		bodyType:   "normal",
 		returncode: 202,
+	}, {
+		name:       "test failure to write to Redis",
+		async:      true,
+		method:     "POST",
+		bodyType:   "failure",
+		returncode: 500,
 	},
 	}
 	for _, test := range tests {
@@ -82,9 +89,13 @@ func TestAsyncRequestHeader(t *testing.T) {
 			setupRedis()
 			request, _ := http.NewRequest(http.MethodGet, testserver.URL, nil)
 			if test.method == "POST" {
-				body := strings.NewReader(`{"body":"this is a body"}`)
-				if test.largeBody == true {
+				var body *strings.Reader
+				if test.bodyType == "normal" {
+					body = strings.NewReader(`{"body":"this is a body"}`)
+				} else if test.bodyType == "large" {
 					body = strings.NewReader(`{"body":"this is a larger body"}`)
+				} else if test.bodyType == "failure" {
+					body = strings.NewReader(`FAIL`)
 				}
 				request, _ = http.NewRequest(http.MethodPost, testserver.URL, body)
 			}
@@ -117,5 +128,8 @@ func setupRedis() {
 }
 
 func (fr *fakeRedis) write(ctx context.Context, s envInfo, reqJSON []byte, id string) (err error) {
+	if strings.Contains(string(reqJSON), "FAIL") {
+		return errors.New("Failure writing")
+	}
 	return // no need to actually write to redis stream for our test case.
 }
